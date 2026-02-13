2bae7e48e1dadce9b90dd94d2c917a4a99653fe4
Christian Buchsteiner <chrbuchsteiner@t-online.de>
2026-02-13 17:58:11 +0100

    initial release 0.001

---
+++ target/debug/deps/extractembedfilepdf-05bb89883f524cad.d
@@ -0,0 +1,5 @@
+/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/extractembedfilepdf-05bb89883f524cad.d: src/main.rs
+
+/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/libextractembedfilepdf-05bb89883f524cad.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/extractembedfilepdf-2cee8bd849d273ce.d
@@ -0,0 +1,5 @@
+/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/extractembedfilepdf-2cee8bd849d273ce.d: src/main.rs
+
+/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/libextractembedfilepdf-2cee8bd849d273ce.rmeta: src/main.rs
+
+src/main.rs:
