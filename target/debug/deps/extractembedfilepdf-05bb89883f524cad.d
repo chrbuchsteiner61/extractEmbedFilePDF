9a06d9bbbc0d53afed58737f455dd20080b3d337
Christian Buchsteiner <chrbuchsteiner@t-online.de>
2026-02-17 16:45:54 +0100

    Remove target directory from git tracking to respect .gitignore

--- target/debug/deps/extractembedfilepdf-05bb89883f524cad.d
+++
@@ -1,5 +0,0 @@
-/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/extractembedfilepdf-05bb89883f524cad.d: src/main.rs
-
-/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/libextractembedfilepdf-05bb89883f524cad.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/extractembedfilepdf-2cee8bd849d273ce.d
+++
@@ -1,5 +0,0 @@
-/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/extractembedfilepdf-2cee8bd849d273ce.d: src/main.rs
-
-/home/christianBuchsteiner/Entwicklung/RustProjekte/extractEmbedFilePDF/target/debug/deps/libextractembedfilepdf-2cee8bd849d273ce.rmeta: src/main.rs
-
-src/main.rs:
